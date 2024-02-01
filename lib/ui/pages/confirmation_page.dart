d2348ca31880d42d3a537c06e75eb771bade0341
Dayveed <dayveed.d@gmail.com>
2024-02-01 06:53:27 -0800

    sms verification

--- lib/ui/pages/confirmation_page.dart
+++ lib/ui/pages/confirmation_page.dart
@@ -7,7 +7,6 @@ import 'package:test_pt/assets/colors.dart';
 import 'my_projects.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 
 class ConfirmationPage extends StatefulWidget {
   const ConfirmationPage({
@@ -22,14 +21,13 @@ class ConfirmationPage extends StatefulWidget {
 }
 
 class _ConfirmationPageState extends State<ConfirmationPage> {
-  late TextEditingController controller;
-
-  final String requiredNumber = '12345';
+  FirebaseAuth auth = FirebaseAuth.instance;
+  String newVerificationId = '';
 
   @override
   void initState() {
     super.initState();
-    controller = TextEditingController();
+    loginWithPhone();
   }
 
   @override
@@ -196,16 +194,20 @@ class _ConfirmationPageState extends State<ConfirmationPage> {
                     activeColor: AppColors.grey_1,
                   ),
                   onCompleted: (value) {
-                    if (value == requiredNumber) {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => MyProjects(),
-                        ),
-                      );
-                    } else {
-                      print('invalid pin');
-                    }
+                    PhoneAuthCredential credential =
+                        PhoneAuthProvider.credential(
+                      verificationId: newVerificationId,
+                      smsCode: value,
+                    );
+                    auth
+                        .signInWithCredential(credential)
+                        .then((value) => _pushToMainPage());
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const MyProjects(),
+                      ),
+                    );
                   },
                 ),
               ),
@@ -215,6 +217,31 @@ class _ConfirmationPageState extends State<ConfirmationPage> {
           ),
         ),
       ),
+    );
+  }
+
+  void _pushToMainPage() => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const MyProjects(),
+        ),
+      );
+
+  void loginWithPhone() async {
+    auth.verifyPhoneNumber(
+      phoneNumber: widget.phoneNumber,
+      verificationCompleted: (PhoneAuthCredential credential) async {
+        await auth.signInWithCredential(credential).then(
+              (value) => _pushToMainPage(),
+            );
+      },
+      verificationFailed: (FirebaseAuthException e) {
+        print(e.message);
+      },
+      codeSent: (String verificationId, int? resendToken) {
+        newVerificationId = verificationId;
+      },
+      codeAutoRetrievalTimeout: (String verificationId) {},
     );
   }
 }
@@ -286,103 +313,5 @@ class _SendCodeTimerState extends State<_SendCodeTimer> {
               ),
             ),
           );
-  }
-}
-
-class LoginWithPhone extends StatefulWidget {
-  const LoginWithPhone({Key? key}) : super(key: key);
-
-  @override
-  _LoginWithPhoneState createState() => _LoginWithPhoneState();
-}
-
-class _LoginWithPhoneState extends State<LoginWithPhone> {
-  TextEditingController phoneController =
-      TextEditingController(text: "+923028997122");
-  TextEditingController otpController = TextEditingController();
-
-  FirebaseAuth auth = FirebaseAuth.instance;
-
-  bool otpVisibility = false;
-
-  String verificationID = "";
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Login With Phone"),
-      ),
-      body: Container(
-        margin: EdgeInsets.all(10),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: phoneController,
-              decoration: InputDecoration(labelText: "Phone number"),
-              keyboardType: TextInputType.phone,
-            ),
-            Visibility(
-              child: TextField(
-                controller: otpController,
-                decoration: InputDecoration(),
-                keyboardType: TextInputType.number,
-              ),
-              visible: otpVisibility,
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  if (otpVisibility) {
-                    verifyOTP();
-                  } else {
-                    loginWithPhone();
-                  }
-                },
-                child: Text(otpVisibility ? "Verify" : "Login")),
-          ],
-        ),
-      ),
-    );
-  }
-
-  void loginWithPhone() async {
-    auth.verifyPhoneNumber(
-      phoneNumber: phoneController.text,
-      verificationCompleted: (PhoneAuthCredential credential) async {
-        await auth.signInWithCredential(credential).then((value) {
-          print("You are logged in successfully");
-        });
-      },
-      verificationFailed: (FirebaseAuthException e) {
-        print(e.message);
-      },
-      codeSent: (String verificationId, int? resendToken) {
-        otpVisibility = true;
-        verificationID = verificationId;
-        setState(() {});
-      },
-      codeAutoRetrievalTimeout: (String verificationId) {},
-    );
-  }
-
-  void verifyOTP() async {
-    PhoneAuthCredential credential = PhoneAuthProvider.credential(
-        verificationId: verificationID, smsCode: otpController.text);
-
-    await auth.signInWithCredential(credential).then((value) {
-      print("You are logged in successfully");
-      Fluttertoast.showToast(
-          msg: "You are logged in successfully",
-          toastLength: Toast.LENGTH_SHORT,
-          gravity: ToastGravity.CENTER,
-          timeInSecForIosWeb: 1,
-          backgroundColor: Colors.red,
-          textColor: Colors.white,
-          fontSize: 16.0);
-    });
   }
 }
